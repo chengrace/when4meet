f1789993ed2151e77158a4eacdd3c88bbad0ec1c
Grace Chen <gracechen@nbp-47-99.nbp.ruw.rutgers.edu>
2022-02-19 14:22:28 -0500

    fixed folder redirect

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='../frontend/public')
 CORS(app) #comment this on deployment
 api = Api(app)
 
