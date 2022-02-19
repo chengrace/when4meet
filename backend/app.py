f697ab1b89a678c9eb14f8b24af82b2495bbdf74
Grace Chen <gracechen@nbp-47-99.nbp.ruw.rutgers.edu>
2022-02-19 14:35:39 -0500

    requirements file

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from HelloApiHandler import HelloApiHandler
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, static_url_path='', static_folder='../frontend/public')
 CORS(app) #comment this on deployment
